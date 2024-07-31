99567dd5b5e057cf43f6ed2eed20b7a7aa98c6d2
Krystie Achsa <krystieachsa@192.168.1.6>
2024-07-31 20:59:49 +0530

    Initial Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,6 @@
   "use strict";
 
   /**
-   * 3D Box rotatation and reset helper function
-   */
-  const box = document.querySelector('.box');
-
-  box.addEventListener('mouseover', () => {
-    box.classList.add('reset'); // Reset box to neutral position
-    setTimeout(() => box.classList.add('pause'), 500); // Add pause class after reset animation
-  });
-  
-  box.addEventListener('mouseout', () => {
-    box.classList.remove('pause', 'reset'); // Remove classes to resume rotation
-  });
-  
-
-  /**
    * Theme selector helper function
    */
   document.addEventListener("DOMContentLoaded", function () {
@@ -288,6 +273,21 @@
       clickable: true
     }
   });
+
+  /**
+   * 3D Box rotatation and reset helper function
+   */
+  const box = document.querySelector('.box');
+
+  box.addEventListener('mouseover', () => {
+    box.classList.add('reset'); // Reset box to neutral position
+    setTimeout(() => box.classList.add('pause'), 500); // Add pause class after reset animation
+  });
+
+  box.addEventListener('mouseout', () => {
+    box.classList.remove('pause', 'reset'); // Remove classes to resume rotation
+  });
+
 
   /**
    * Initiate Pure Counter 
