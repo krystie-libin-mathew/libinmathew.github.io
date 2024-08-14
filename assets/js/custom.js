64ad6c27f2f6ac62e38f48f378e5b0a2659ccd20
Krystie Achsa <krystieachsa@Krysties-MacBook-Pro.local>
2024-08-14 16:12:04 +0400

    Initial Commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,11 +9,43 @@
     box.addEventListener('mouseover', () => {
         box.classList.add('pause'); // Pause the rotation
     });
-    
+
     box.addEventListener('mouseout', () => {
         box.classList.remove('pause'); // Resume the rotation
     });
 
+
+    /**
+    * Color Box position on top and toggle helper function
+    */
+    const togglePalette = document.getElementById('toggle-palette');
+    const themePalette = document.getElementById('theme-palette');
+
+    function togglePaletteVisibility() {
+        themePalette.classList.toggle('collapsed');
+        // const isCollapsed = themePalette.classList.contains('collapsed');
+        // togglePalette.querySelector('span').innerHTML = isCollapsed ? '&#9660;' : '&#9650;'; // Change arrow direction
+    }
+
+    // Initialize with palette visible or hidden based on your preference
+    themePalette.classList.add('collapsed'); // Start hidden
+
+    togglePalette.addEventListener('click', togglePaletteVisibility);
+
+    document.querySelectorAll('.theme-button').forEach(button => {
+        button.addEventListener('click', function () {
+            const theme = this.getAttribute('data-theme');
+            document.documentElement.setAttribute('data-theme', theme);
+
+            // Collapse the palette after a theme is selected
+            themePalette.classList.add('collapsed');
+            togglePalette.querySelector('span').innerHTML = '🎨'; // Change to down arrow
+        });
+    });
+
+    /**
+    * Navbar Switch from Vertical to Horizontal helper function
+    */
     document.addEventListener('DOMContentLoaded', function () {
         const navbar = document.querySelector('#navbar');
         const navLinks = document.querySelectorAll('.navbar a');
